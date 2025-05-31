9fa48b8691e2902101004519fd1e5bdd2d4f503c
Shashwat <80104797+amazinglySK@users.noreply.github.com>
2025-05-31 15:43:44 +0530

    Added some beta testing auth
    A mini auth function for beta testing preview. Auth code loaded from
    environment variables

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import type { Handle } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit'
+import { compare } from 'bcrypt'
+import { ACCESS_CODE } from '$env/static/private'
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const protected_check =
+    event.route?.id?.startsWith('/(protected)') && import.meta.env.DEV
+
+  if (protected_check) {
+    if (!event.cookies.get('access')) throw redirect(303, '/')
+    const sesh = event.cookies.get('session_token') || ''
+    const result = await compare(ACCESS_CODE, sesh)
+    if (!result) throw redirect(303, '/')
+  }
+  const response = await resolve(event)
+  return response
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import '../app.css'
-  export const ssr = false
-  export const csr = true
   let { children } = $props()
 </script>
 
